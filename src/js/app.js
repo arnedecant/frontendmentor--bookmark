a9db5a13396994bf8fdc9c92c7d55410670aa0df
Arne Decant <hello@arnedecant.be>
2019-10-26 23:27:54 +0200

    cta & footer

--- src/js/app.js
+++ src/js/app.js
@@ -14,19 +14,33 @@ class App {
 
 		this.init()
 
+		document.addEventListener('click', this.click.bind())
+
 	}
 
 	init(e) {
+
+		// Init components
 
 		this.accordion = new Accordion('[data-component="accordion"]')
 		this.tabs = new Tabs('[data-component="tabs"]')
 		this.form = new Form('[data-component="form"]')
 
+		// TODO: Check current browser and update button order accordingly
+		// Chrome first if the user is using Chrome
+		// Firefox first if the user is using Firefox
+
+		// TODO: Paralax-like effect on the hero illustration
+		// Update the blue background and the illustration itself,
+		// according to mouse position, with a different multiplier
+
 	}
 
 	click(e) {
 
-		
+		// Disable dummy anchors
+
+		if (e.target.href.endsWith('#')) e.preventDefault()
 
 	}
 

25fc7e4ae43b2ac898be2726b552efe4d5b03a31
Arne Decant <hello@arnedecant.be>
2019-10-28 20:30:22 +0100

    init smooth scrolling

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,10 @@
 // :: App
 // -------------------------------------------------------------------
 
-import Accordion from "./components/accordion"
+import Menu from "./components/menu"
 import Tabs from "./components/tabs"
 import Form from "./components/form"
+import Accordion from "./components/accordion"
 
 class App {
 
@@ -19,33 +20,67 @@ class App {
 
 		// Init components
 
-		this.menu = new Accordion('[data-component="menu"]')
-		this.accordion = new Accordion('[data-component="accordion"]')
+		this.menu = new Menu('[data-component="menu"]')
 		this.tabs = new Tabs('[data-component="tabs"]')
 		this.form = new Form('[data-component="form"]')
+		this.accordion = new Accordion('[data-component="accordion"]')
 
 		// TODO: Check current browser and update button order accordingly
 		// Chrome first if the user is using Chrome
 		// Firefox first if the user is using Firefox
 
+		// TODO: Scroll-to animation onclick nav link
+		// Scroll animation to the section of the page
+
+		// TODO: Animate-in effects whenever a section gets in view
+		// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
 		// TODO: Paralax-like effect on the hero illustration
 		// Update the blue background and the illustration itself,
 		// according to mouse position, with a different multiplier
 
-		// TODO: Check OS color scheme and apply accordingly
+		// TODO: Check OS color scheme and apply accordingly (dark / light)
 		// Perhaps use CSS: @media (prefers-color-scheme: dark) { ... }
 
-		// TODO: Show modal (new compoennt...) whenever a dummy link is clicked
-		// Model shows info about me, the project and also a link to the GitHub
-		// repository and perhaps maybe a blogpost about the codebase..?
+		// TODO: Show modal (new component...) whenever a dummy link is clicked
+		// Modal shows info about me, the project and also a link to the GitHub
+		// repository and perhaps maybe a link to a blogpost about the codebase..?
 
 	}
 
 	click(e) {
 
+		// NOTE: e.target.href is a converted string, including domain
+		// e.target.getAttribute('href) gets the actual value as written in HTML
+
+		const href = e.target.getAttribute('href')
+
 		// Disable dummy anchors
 
-		if (e.target.href && e.target.href.endsWith('#')) e.preventDefault()
+		if (href) e.preventDefault()
+		if (href == '#') return
+
+		// Smooth scroll-to animation onclick nav link
+		
+		const $section = document.querySelector(href)
+		if ($section.length) this.scrollIntoView($section)
+
+	}
+
+	scrollIntoView(el) {
+
+		// NOTE: alternative method: element.scrollIntoView({ behavior: 'smooth' })
+		// only works when element is a direct child of the body element. As this
+		// Web API only provides for the parent to scroll. What we have is 
+		// body > main > section, which would scroll the main element.
+
+		// el.scrollIntoView({ behavior: 'smooth' })
+
+		// window.scroll({
+		// 	top: 2500, 
+		// 	left: 0, 
+		// 	behavior: 'smooth'
+		// })
 
 	}
 

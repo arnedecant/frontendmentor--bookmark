c3b9b8f865c3db1690b9bd890604b3a16705a65d
Arne Decant <hello@arnedecant.be>
2019-11-02 20:33:17 +0100

    mobile menu component

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -6,9 +6,13 @@ import Component from './component'
 
 export default class Menu extends Component {
 
-	constructor(selector) {
+	constructor(selector, hamburger = '[data-toggle="menu"]') {
 
 		super(selector)
+
+		this.$button = document.querySelector(hamburger)
+		
+		this.$button.addEventListener('click', this.click.bind(this))
 
 	}
 
@@ -20,7 +24,12 @@ export default class Menu extends Component {
 
 	click(e) {
 
-		
+		if (!e.target.dataset.toggle) return
+		if (e.target.nodeName !== 'BUTTON' && e.target.nodeName !== 'A') return
+
+		this.$button.classList.toggle('active')
+		this.element.classList.toggle('active')
+
 
 	}
 
